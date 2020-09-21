702ab395619ccca340cc1601d16d83a0400413a8
Ermis PAPASTEFANAKIS <ermisp@gmail.com>
2020-09-21 15:28:13 +0200

    Updates

---
+++ precache-manifest.9a1b331e34010fe1072a7672ce17b987.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a1f8386e60c696e5bd2ced790b6db10",
+    "url": "/testleaf.github.io/index.html"
+  },
+  {
+    "revision": "96fe259ea017ed96a8ad",
+    "url": "/testleaf.github.io/static/css/main.46000bd8.chunk.css"
+  },
+  {
+    "revision": "604a13b50baaa270646e",
+    "url": "/testleaf.github.io/static/js/2.d2beb660.chunk.js"
+  },
+  {
+    "revision": "7414a271e33e63201c0af477443fe765",
+    "url": "/testleaf.github.io/static/js/2.d2beb660.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96fe259ea017ed96a8ad",
+    "url": "/testleaf.github.io/static/js/main.e64fe228.chunk.js"
+  },
+  {
+    "revision": "898fd350cb6daf6dee06",
+    "url": "/testleaf.github.io/static/js/runtime-main.c8e2fe71.js"
+  }
+]);
